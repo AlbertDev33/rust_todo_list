9e9ba15203c900e3cc44b651415892085a2585d3
Albert <albert.rocha@r2t.company>
2022-12-06 17:00:16 -0300

    feature: get_todos method created

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,8 +1,22 @@
-use actix_web::{HttpResponse, Responder};
+use actix_web::{web, HttpResponse, Responder};
+use deadpool_postgres::{Pool, Client};
+
 use crate::models::Status;
+use crate::db;
 
 pub async fn status() -> impl Responder {
     HttpResponse::Ok().json(Status {
         status: "Ok".to_string(),
     })
+}
+
+pub async fn get_todos(db_pool: web::Data<Pool>) -> impl Responder {
+    let client: Client = db_pool.get().await.expect("Error connecting to the database");
+
+    let result = db::get_todos(&client).await;
+    
+    match result {
+        Ok(todos) => HttpResponse::Ok().json(todos),
+        Err(_) => HttpResponse::InternalServerError().into()
+    }
 }
